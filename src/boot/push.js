d226b6629b56e56fdff4509ac525dc400143c77c
Meidanmor <meidanmuzrafi@gmail.com>
2025-06-03 18:32:54 +0300

    add console.log to sw

--- src/boot/push.js
+++ src/boot/push.js
@@ -1,16 +1,26 @@
 export async function subscribeToPushNotifications() {
+  console.log('🚀 Push setup started');
+
   const permission = await Notification.requestPermission();
+  console.log('🟢 Permission result:', permission);
+
   if (permission !== 'granted') {
-    console.warn('Notification permission not granted.');
+    console.warn('🔴 Notification permission not granted.');
     return;
   }
 
   try {
+    console.log('🔄 Waiting for service worker');
     const registration = await navigator.serviceWorker.ready;
+
+    console.log('🟢 Service worker ready:', registration);
+
     const subscription = await registration.pushManager.subscribe({
       userVisibleOnly: true,
       applicationServerKey: 'BLDC7BhdZ5IEKTcBzhXz2jLkiaQvxpL8hjb-uVFIomYEArnRKhEvAIlDbHAiOgJwaj9IwHAJUD-p8POw0VCowYg'
     });
+
+    console.log('📝 Push subscription object:', subscription);
 
     const res = await fetch('https://nuxt.meidanm.com/wp-json/pwa/v1/save-subscription', {
       method: 'POST',
@@ -19,9 +29,9 @@ export async function subscribeToPushNotifications() {
     });
 
     const result = await res.json();
-    console.log('Push subscription response:', result);
+    console.log('✅ Push subscription saved:', result);
   } catch (err) {
-    console.error('Push subscription failed:', err);
+    console.error('❌ Push subscription failed:', err);
   }
 }
 
@@ -30,3 +40,4 @@ if ('Notification' in window && 'serviceWorker' in navigator && 'PushManager' in
 } else {
   console.warn('Push notifications are not supported in this browser.');
 }
+
