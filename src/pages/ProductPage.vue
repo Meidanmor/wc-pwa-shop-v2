a108667a34223c6fee460365ac686dbbdf4e504a
Meidanmor <meidanmuzrafi@gmail.com>
2025-06-08 15:34:18 +0300

    FIX SEO

--- src/pages/ProductPage.vue
+++ src/pages/ProductPage.vue
@@ -502,17 +502,6 @@ console.log(selectedVariation.value ? selectedVariation.value.id : product.value
 }
 
 onMounted(async() => {
-  try {
-    const res = await fetch(`https://nuxt.meidanm.com/wp-json/custom/v1/seo?path=${encodeURIComponent(route.fullPath)}`)
-    const data = await res.json()
-    seoData.value = {
-      title: data.title || 'Fallback Title',
-      description: data.description || 'Fallback description'
-    }
-    console.log('[CSR] Fetched SEO:', seoData.value.title)
-  } catch (err) {
-    console.error('[CSR] SEO Fetch Error:', err)
-  }
   fetchProduct(route.params.slug);
   //fetchWishlistData()
 })
