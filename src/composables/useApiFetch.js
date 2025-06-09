33948d03eec22de471d28901f4dfb9cb356c8567
Meidanmor <meidanmuzrafi@gmail.com>
2025-06-09 09:30:05 +0300

    Fix token JWT

--- src/composables/useApiFetch.js
+++ src/composables/useApiFetch.js
@@ -1,14 +1,20 @@
 // src/composables/useApiFetch.js
+function isValidJwt(token) {
+  return typeof token === 'string'
+    && token.trim() !== ''
+    && token !== 'null'
+    && token !== 'undefined'
+    && token.split('.').length === 3
+}
 export async function fetchWithToken(url, options = {}) {
   if (process.env.CLIENT) {
     const token = localStorage.getItem('jwt_token') // or use Vue state if stored globally
 
-    const isValidToken = token && typeof token === 'string' && token.trim().split('.').length === 3;
 
     console.log(token);
     const headers = {
       'Content-Type': 'application/json',
-      ...(isValidToken ? {Authorization: `Bearer ${token}`} : {})
+      ...(isValidJwt(token) ? {Authorization: `Bearer ${token}`} : {})
     }
 
     return fetch(url, {
