13df56ac0d3c310dcf3992bbe1a975287e8be865
Meidanmor <meidanmuzrafi@gmail.com>
2025-07-03 10:04:48 +0300

    add csp for google login

--- src-ssr/middlewares/csp.js
+++ src-ssr/middlewares/csp.js
@@ -1,6 +1,11 @@
 export default async ({ /*req,*/ res }) => {
-    res.setHeader(
+    /*res.setHeader(
         'Content-Security-Policy',
         "default-src *; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+    )*/
+    res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self' https://accounts.google.com; object-src 'none'"
     )
+
 }
