67bcee1a5c09b21719049e7fe5aedfe9198c3feb
Meidanmor <meidanmuzrafi@gmail.com>
2025-07-04 14:27:22 +0300

    add credentials to fetch

--- src/components/GoogleLoginButton.vue
+++ src/components/GoogleLoginButton.vue
@@ -39,6 +39,7 @@ function handleCredentialResponse(response) {
 
   fetch('https://nuxt.meidanm.com/wp-json/custom/v1/google-login', {
     method: 'POST',
+    credentials: 'include',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ token: idToken }),
   })
