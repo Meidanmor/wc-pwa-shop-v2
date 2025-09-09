e6083f8ffa9721cc72c1593877e2b9f93aa7b548
Meidanmor <meidanmuzrafi@gmail.com>
2025-09-09 08:18:26 +0300

    fix headers error from line break

--- src-ssr/middlewares/render.js
+++ src-ssr/middlewares/render.js
@@ -2,37 +2,35 @@
 import { defineSsrMiddleware } from '#q-app/wrappers'
 
 export default defineSsrMiddleware(({ app, resolve, render, serve }) => {
-  app.get(resolve.urlPath('*'), (req, res) => {
-    res.setHeader('Content-Type', 'text/html')
+    app.get(resolve.urlPath('*'), (req, res) => {
+        res.setHeader('Content-Type', 'text/html')
 
-    // CSP header matching your htmlVariables settings
-    res.setHeader(
-      'Content-Security-Policy',
-      `
-        default-src 'self';
-        script-src 'self' https://accounts.google.com 'unsafe-inline';
-        style-src 'self' 'unsafe-inline';
-        img-src 'self' data: https:;
-        connect-src 'self' https://nuxt.meidanm.com;
-        font-src 'self' https://fonts.gstatic.com;
-        frame-src https://accounts.google.com;
-      `
-    )
+        // CSP header matching your htmlVariables settings
+        res.setHeader(
+            'Content-Security-Policy',
+            "default-src 'self'; " +
+            "script-src 'self' https://accounts.google.com 'unsafe-inline'; " +
+            "style-src 'self' 'unsafe-inline'; " +
+            "img-src 'self' data: https:; " +
+            "connect-src 'self' https://nuxt.meidanm.com; " +
+            "font-src 'self' https://fonts.gstatic.com; " +
+            "frame-src https://accounts.google.com;"
+        )
 
-    render({ req, res })
-      .then(html => res.send(html))
-      .catch(err => {
-        if (err.url) {
-          if (err.code) res.redirect(err.code, err.url)
-          else res.redirect(err.url)
-        } else if (err.code === 404) {
-          res.status(404).send('404 | Page Not Found')
-        } else if (process.env.DEV) {
-          serve.error({ err, req, res })
-        } else {
-          res.status(500).send('500 | Internal Server Error')
-          if (process.env.DEBUGGING) console.error(err.stack)
-        }
-      })
-  })
+        render({req, res})
+            .then(html => res.send(html))
+            .catch(err => {
+                if (err.url) {
+                    if (err.code) res.redirect(err.code, err.url)
+                    else res.redirect(err.url)
+                } else if (err.code === 404) {
+                    res.status(404).send('404 | Page Not Found')
+                } else if (process.env.DEV) {
+                    serve.error({err, req, res})
+                } else {
+                    res.status(500).send('500 | Internal Server Error')
+                    if (process.env.DEBUGGING) console.error(err.stack)
+                }
+            })
+    })
 })
