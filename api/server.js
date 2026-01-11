3a5fc2859fa2e5d395ee511079cfc35dc30213d1
Meidanmor <meidanmuzrafi@gmail.com>
2026-01-11 11:59:20 +0200

    update vercel settings

--- api/server.js
+++ api/server.js
@@ -1,11 +1,6 @@
 // api/index.js
-export default async function handler(req, res) {
-  try {
-    // We import the compiled SSR entry from the dist folder
-    const { handler } = await import('../dist/ssr/index.js');
-    return handler(req, res);
-  } catch (error) {
-    console.error('Vercel Bridge Error:', error);
-    res.status(500).send('Internal Server Error: ' + error.message);
-  }
+import { handler } from '../dist/ssr/index.js';
+
+export default function (req, res) {
+  return handler(req, res);
 }
