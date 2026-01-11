6d9fb56323b28eeb6db7d3c16423d714b7d2c99e
Meidanmor <meidanmuzrafi@gmail.com>
2026-01-11 12:04:33 +0200

    update vercel settings

--- api/server.js
+++ api/server.js
@@ -1,6 +1,12 @@
 // api/index.js
 import { handler } from '../dist/ssr/index.js';
 
-export default function (req, res) {
-  return handler(req, res);
+export default async function (req, res) {
+  try {
+    // Quasar handler is an (req, res) Express-like function
+    return handler(req, res);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 }
